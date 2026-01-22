678c0fec1712c69ef4cab38c33871bf35ddfc927
Matthew <venotics@gmail.com>
2026-01-22 13:23:41 -0600

    Initial Commit

---
+++ Chatroom/ChatroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatroomApp.swift
+//  Chatroom
+//
+//  Created by Matthew Infante on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatroomTests/ChatroomTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatroomTests.swift
+//  ChatroomTests
+//
+//  Created by Matthew Infante on 1/22/26.
+//
+
+import Testing
+@testable import Chatroom
+
+struct ChatroomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
